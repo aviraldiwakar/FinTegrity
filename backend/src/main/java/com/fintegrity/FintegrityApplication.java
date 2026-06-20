7c37de782b3226bb7fd9872adf5f312f3b666b19
Aviral Jain <aviralj193@gmail.com>
2026-06-20 16:32:11 +0530

    Added missing Spring Boot main class

---
+++ backend/src/main/java/com/fintegrity/FintegrityApplication.java
@@ -0,0 +1,11 @@
+package com.fintegrity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintegrityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FintegrityApplication.class, args);
+    }
+}
